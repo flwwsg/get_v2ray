890b40be39f74c2457de7e2504009598938835fa
flwwsg <2319406132@qq.com>
2019-03-13 19:51:09 +0800

    处理页面刷新

--- src/v2ray.py
+++ src/v2ray.py
@@ -1,11 +1,15 @@
 # 获取v2ray代理账号
 import os
 from utils import v2ray_dir, free_config, web_driver, v2ray_program, need_root_permission
-
+import time
 
 config_file = v2ray_dir+"/config.json"
 # 获取配置，免费账号每天会更新
 web_driver.get(free_config)
+# 处理ddos
+time.sleep(6)
+# 刷新页面，获取数据
+web_driver.refresh()
 # 通过chrome获取json数据
 chrome_json = web_driver.find_elements_by_tag_name("pre")
 # 通过firefox获取json数据
